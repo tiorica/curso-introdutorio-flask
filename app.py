2c170738da09ffd3d36bf684a2b2828a53db9be9
ricardo.poffo <ricardo.poffo@dataprev.gov.br>
2024-01-19 20:07:23 +0000

    Aula 02 1:15:37

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+# Rota raiz
+@app.route('/')
+def hello_world():
+    return 'Oi imundo!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
