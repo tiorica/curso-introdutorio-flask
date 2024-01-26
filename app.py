8bb549b2c8ac1ca4cee4de0583d532500790665b
Ricardo Poffo <tiorica@gmail.com>
2024-01-26 17:26:44 -0300

    Add routes: get, update, all . 2:30:00

--- app.py
+++ app.py
@@ -36,7 +36,54 @@ def delete_product(product_id):
         db.session.delete(product)
         db.session.commit()
         return jsonify({"message": "Product removed from database"}), 200
-    return jsonify({"message": "Product not found"}), 400
+    return jsonify({"message": "Product not found"}), 404
+
+# Get product detail route
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "price": product.price,
+            "name": product.name,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"}), 404
+
+# Update product detail route
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+        
+    if 'price' in data:
+        product.price = data['price']
+        
+    if 'description' in data:
+        product.description = data['description']
+    
+    db.session.commit()
+    return jsonify({"message": "Product updated"})
+
+# Get all products route
+@app.route('/api/products', methods=["GET"])
+def get_all_products():
+    response_product_list = Product.query.all()
+    product_list = []
+    for product in response_product_list:
+        product_data = {
+            "id": product.id,
+            "name": product.name
+        }
+        product_list.append(product_data)
+    
+    return jsonify(product_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
