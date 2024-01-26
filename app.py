7d07ec46f7b35fdb30d5269f71802f128400123c
Ricardo Poffo <tiorica@gmail.com>
2024-01-26 15:39:21 -0300

    Routes add, delete product. 1:55:00

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,6 +16,27 @@ class Product(db.Model):
 @app.route('/')
 def hello_world():
     return 'Oi imundo!'
+
+# Add product route
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description",""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product inserterd in Database"}), 200 
+    return jsonify({"message": "Invalid product data"}), 400
+
+# Delete product route
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product removed from database"}), 200
+    return jsonify({"message": "Product not found"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
